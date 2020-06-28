fa08e6b8336ab6ef9d203e366425359221405d2c
artem <mstrJesm@gmail.com>
2020-06-28 22:53:37 +0300

    Added Search and Cart components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Profile from './pages/Profile/Profile';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+import Navbar from './components/Navbar/Navbar';
+
 
 function App() {
   return (
-    <div className="container pt-4">
-      <h1>Hello world</h1>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+      <div className="container pt-4">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </div>
+    </Router>
+    </>
   );
 }
 
