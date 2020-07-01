3f96dd357c3d063a0df0f8e009496521fe41861c
artem <mstrJesm@gmail.com>
2020-07-01 23:15:00 +0300

    added github context funcs

--- src/App.js
+++ src/App.js
@@ -6,21 +6,28 @@ import Profile from './pages/Profile/Profile';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
-
+import Alert from './components/Alert/Alert';
+import {AlertState} from './context/alert/AlertState';
+import {GithubState} from './context/github/GithubState';
 
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
-      <div className="container pt-4">
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/profile" component={Profile} />
-        </Switch>
-      </div>
-    </Router>
+    <GithubState>
+      <AlertState>
+        <Router>
+          <Navbar />
+          <div className="container pt-4">
+            <Alert alert={{text: 'Test alert'}}/>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/profile" component={Profile} />
+            </Switch>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
     </>
   );
 }
